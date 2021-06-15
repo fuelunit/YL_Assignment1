c5db9a402ad913f4c42bf78fb759071b94f51ec7
fuelunit <yipeng.liu@sjsu.edu>
2021-06-15 15:29:58 -0700

    Second

---
+++ src/edu/sjsu/assignment1/Matrix.java
@@ -0,0 +1,29 @@
+/*
+    Author: Yipeng Liu
+    Project: Assignment 1
+    Class: Matrix
+    Date: 06/15/2021
+ */
+package edu.sjsu.assignment1;
+
+public class Matrix {
+    /**
+     *
+     * @param matrix
+     */
+    public Matrix(int[][] matrix) {
+
+    }
+
+    /**
+     * The {@code multiply} method takes another Matrix as a parameter,
+     * computes the matrix multiplication (between the current Matrix
+     * and the parameter Matrix), and returns the resulting Matrix.
+     * @param otherMatrix
+     * @return
+     */
+    public Matrix multiply(Matrix otherMatrix) {
+        Matrix result = new Matrix(null);
+        return result;
+    }
+}
---
+++ src/edu/sjsu/assignment1/MySum.java
@@ -0,0 +1,19 @@
+/*
+    Author: Yipeng Liu
+    Project: Assignment 1
+    Class: MySum
+    Date: 06/15/2021
+ */
+package edu.sjsu.assignment1;
+
+public class MySum {
+
+    /**
+     * A public static function that takes an integer array and returns
+     * the alternating sum (an integer) of all elements in the array.
+     */
+    public static int getAlternatingSum(int[] array) {
+
+        return 0;
+    }
+}
