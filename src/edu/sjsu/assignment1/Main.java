21dc8d6839fe3bb25c363b2e2b374d3493e51299
fuelunit <yipeng.liu@sjsu.edu>
2021-06-17 14:01:40 -0700

    5th commit for creating a Main class

---
+++ src/edu/sjsu/assignment1/Main.java
@@ -0,0 +1,23 @@
+/*
+    Author: Yipeng Liu
+    Project: Assignment 1
+    Class: Main
+    Date: 06/17/2021
+ */
+package edu.sjsu.assignment1;
+
+public class Main {
+    public static void main(String[] args) {
+        MyTime.printTimeDifference("0120", "1510");
+        MyTime.printTimeDifference("1635", "0250");
+        Card card1 = new Card("10S");
+        String fullDescription = card1.getDescription();
+        System.out.println(fullDescription);
+        Card card2 = new Card("QC");
+        fullDescription = card2.getDescription();
+        System.out.println(fullDescription);
+        Card card3 = new Card("11H");
+        fullDescription = card3.getDescription();
+        System.out.println(fullDescription);
+    }
+}
