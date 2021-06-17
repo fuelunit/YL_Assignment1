8151236066e4694e3ae3194d7418682dd0c80912
fuelunit <yipeng.liu@sjsu.edu>
2021-06-17 13:51:47 -0700

    Third

--- src/edu/sjsu/assignment1/Card.java
+++ src/edu/sjsu/assignment1/Card.java
@@ -19,7 +19,7 @@ public class Card {
      */
     public Card(String cardStr) {
         if (cardStr.length() == 2 || cardStr.length() == 3) {
-            this.setCard();
+            this.setCard(cardStr);
         } else {
             this.rank = UNKNOWN;
             this.suit = UNKNOWN;
@@ -29,9 +29,61 @@ public class Card {
 
     /**
      * Sets the {@code Card} instances in the constructor.
+     * @param cardInfoStr a {@code Card} string
      */
-    private void setCard() {
-
+    private void setCard(String cardInfoStr) {
+        String cardRank;
+        String cardSuit;
+        if (cardInfoStr.length() == 2) {
+            // String slicing
+            cardRank = cardInfoStr.substring(0, 1);
+            cardSuit = cardInfoStr.substring(1);
+            // Setting Card rank
+            switch (cardRank) {
+                case "A" -> this.rank = "Ace";
+                case "2" -> this.rank = "Two";
+                case "3" -> this.rank = "Three";
+                case "4" -> this.rank = "Four";
+                case "5" -> this.rank = "Five";
+                case "6" -> this.rank = "Six";
+                case "7" -> this.rank = "Seven";
+                case "8" -> this.rank = "Eight";
+                case "9" -> this.rank = "Nine";
+                case "J" -> this.rank = "Jack";
+                case "Q" -> this.rank = "Queen";
+                case "K" -> this.rank = "King";
+                default -> {
+                    this.rank = UNKNOWN;
+                    this.description = UNKNOWN;
+                }
+            }
+        } else { // cardInfoStr.length() == 3
+            // String slicing
+            cardRank = cardInfoStr.substring(0, 2);
+            cardSuit = cardInfoStr.substring(2);
+            // Setting Card rank
+            if (cardRank.equals("10")) {
+                this.rank = "Ten";
+            } else {
+                this.rank = UNKNOWN;
+                this.description = UNKNOWN;
+            }
+        }
+        // Setting Card suit
+        switch (cardSuit) {
+            case "D" -> this.suit = "Diamonds";
+            case "H" -> this.suit = "Hearts";
+            case "S" -> this.suit = "Spades";
+            case "C" -> this.suit = "Clubs";
+            default -> {
+                this.suit = UNKNOWN;
+                this.description = UNKNOWN;
+            }
+        }
+        // Setting Card Description
+        if (this.description != UNKNOWN) {
+            this.description = this.rank + " of " + this.suit;
+        }
     }
 
     public String getRank() {
--- src/edu/sjsu/assignment1/MyTime.java
+++ src/edu/sjsu/assignment1/MyTime.java
@@ -11,8 +11,14 @@ public class MyTime {
     public static void main(String[] args) {
         printTimeDifference("0120", "1510");
         printTimeDifference("1635", "0250");
-        Card card = new Card("10S");
-        String fullDescription = card.getDescription();
+        Card card1 = new Card("10S");
+        String fullDescription = card1.getDescription();
+        System.out.println(fullDescription);
+        Card card2 = new Card("QC");
+        fullDescription = card2.getDescription();
+        System.out.println(fullDescription);
+        Card card3 = new Card("11H");
+        fullDescription = card3.getDescription();
         System.out.println(fullDescription);
     }
 
