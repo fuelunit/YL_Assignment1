2e9396d61ef3deb7c64aa4e033fdd881b8acae39
fuelunit <yipeng.liu@sjsu.edu>
2021-06-22 13:24:21 -0700

    10th commit: Implemented getAlternatingSum

--- src/edu/sjsu/assignment1/Main.java
+++ src/edu/sjsu/assignment1/Main.java
@@ -8,11 +8,13 @@ package edu.sjsu.assignment1;
 
 public class Main {
     public static void main(String[] args) {
+        // MyTime
         MyTime.printTimeDifference("0120", "1510");
         MyTime.printTimeDifference("1635", "0250");
         MyTime.printTimeDifference("1820", "1810");
         MyTime.printTimeDifference("1810", "1820");
         MyTime.printTimeDifference("1810", "1810");
+        // Card
         Card card1 = new Card("10S");
         String fullDescription = card1.getDescription();
         System.out.println(fullDescription);
@@ -22,5 +24,10 @@ public class Main {
         Card card3 = new Card("11H");
         fullDescription = card3.getDescription();
         System.out.println(fullDescription);
+        // MySum
+        int result = MySum.getAlternatingSum(new int[]{1, 4, 9, 16, 9, 7, 4, 9, 11});
+        System.out.println(result);
+        result = MySum.getAlternatingSum(new int[]{4, 2, 3, 4, 5, 6});
+        System.out.println(result);
     }
 }
--- src/edu/sjsu/assignment1/MySum.java
+++ src/edu/sjsu/assignment1/MySum.java
@@ -11,9 +11,22 @@ public class MySum {
     /**
      * A public static function that takes an integer array and returns
      * the alternating sum (an integer) of all elements in the array.
+     *
+     * @param array an array of integers
      */
     public static int getAlternatingSum(int[] array) {
-
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                // if i is even: 0, 2, 4, etc., add the number
+                // at index i to sum
+                sum += array[i];
+            } else {
+                // if i is odd, subtract the number
+                // from the sum
+                sum -= array[i];
+            }
+        }
+        return sum;
     }
 }
