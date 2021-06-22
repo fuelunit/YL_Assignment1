9e87b5d3cbba8fbae18da1d889518db7743931f2
fuelunit <yipeng.liu@sjsu.edu>
2021-06-22 14:20:43 -0700

    11th commit: Implemented Matrix class with methods

--- src/edu/sjsu/assignment1/Main.java
+++ src/edu/sjsu/assignment1/Main.java
@@ -29,5 +29,20 @@ public class Main {
         System.out.println(result);
         result = MySum.getAlternatingSum(new int[]{4, 2, 3, 4, 5, 6});
         System.out.println(result);
+        // Matrix
+        int[][] array1 = new int[][]{{2, 4}, {3, 4}};
+        int[][] array2 = new int[][]{{1, 2}, {1, 3}};
+        Matrix matrix1 = new Matrix(array1);
+        Matrix matrix2 = new Matrix(array2);
+        Matrix resultMatrix = matrix1.multiply(matrix2);
+        int[][] resultArray = resultMatrix.getMatrix();
+        Matrix.printMatrix(resultMatrix);
+        int[][] array3 = new int[][]{{1, 2, 3}, {4, 5, 6}};
+        int[][] array4 = new int[][]{{7, 8}, {9, 10}, {11, 12}};
+        Matrix matrix3 = new Matrix(array3);
+        Matrix matrix4 = new Matrix(array4);
+        Matrix resultMatrix2 = matrix3.multiply(matrix4);
+        int[][] resultArray2 = resultMatrix2.getMatrix();
+        Matrix.printMatrix(resultMatrix2);
     }
 }
--- src/edu/sjsu/assignment1/Matrix.java
+++ src/edu/sjsu/assignment1/Matrix.java
@@ -6,28 +6,70 @@
  */
 package edu.sjsu.assignment1;
 
-public class Matrix {
-    /**
-     *
-     * @param matrix
-     */
-    public Matrix(int[][] matrix) {
+import java.util.Arrays;
 
+public class Matrix {
+    private int[][] matrix;
+
+    /**
+     * The constructor takes a 2-d integer array.
+     *
+     * @param inputMatrix a 2-d integer array
+     */
+    public Matrix(int[][] inputMatrix) {
+        this.matrix = new int[inputMatrix.length][inputMatrix[0].length];
+        for (int i = 0; i < inputMatrix.length; i++) {
+            for (int j = 0; j < inputMatrix[i].length; j++) {
+                this.matrix[i][j] = inputMatrix[i][j];
+            }
+        }
     }
 
     /**
      * The {@code multiply} method takes another Matrix as a parameter,
      * computes the matrix multiplication (between the current Matrix
      * and the parameter Matrix), and returns the resulting Matrix.
-     * @param otherMatrix
-     * @return
+     *
+     * @param otherMatrix a {@code Matrix}
+     * @return result a {@code Matrix}
      */
     public Matrix multiply(Matrix otherMatrix) {
-        Matrix result = new Matrix(null);
-        return result;
+        if (otherMatrix.getMatrix().length != this.matrix[0].length) {
+            return null;
+        }
+        int resultRows = this.matrix.length;
+        int resultColumns = otherMatrix.getMatrix()[0].length;
+        int[][] tempResult = new int[resultRows][resultColumns];
+        // Populating tempResult
+        for (int i = 0; i < resultRows; i++) {
+            for (int j = 0; j < resultColumns; j++) {
+                // Getting the product for tempResult[i][j]
+                int product = 0;
+                for (int s = 0; s < this.matrix[i].length; s++) {
+                    product += this.matrix[i][s]
+                            * otherMatrix.getMatrix()[s][j];
+                }
+                tempResult[i][j] = product;
+            }
+        }
+        return new Matrix(tempResult);
     }
 
     public int[][] getMatrix(){
-        return null;
+        return this.matrix;
+    }
+
+    public static void printMatrix(Matrix inputMatrix) {
+        int rows = inputMatrix.getMatrix().length;
+        int columns = inputMatrix.getMatrix()[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(inputMatrix.getMatrix()[i][j]);
+                if (j < columns - 1) {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
     }
 }
